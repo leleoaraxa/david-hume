99ca8e242d70955beb9453bc10a98ecea58eca62
SiRiOS <sirios@sirios.com.br>
2024-08-05 11:24:51 -0300

    Fundamentos App Router - Suspense API no React

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <h1>Hello World!</h1>
+import { GithubProfile } from '@/components/github-profile'
+import { LongWaitComponent } from '@/components/long-wait-component'
+import { Suspense } from 'react'
+
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  return (
+    <div>
+      <h1>Home!</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum provident
+        recusandae quas magnam distinctio adipisci officiis sapiente molestiae
+        libero, illo ut deserunt velit fugiat, temporibus veniam, pariatur ipsam
+        obcaecati. Hic!
+      </p>
+
+      <Suspense fallback={<p>Carregando LongWaitComponent</p>}>
+        <LongWaitComponent />
+      </Suspense>
+
+      <Suspense fallback={<p>Carregando GithubProfile</p>}>
+        <GithubProfile />
+      </Suspense>
+    </div>
+  )
 }
